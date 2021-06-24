1b85d839bfc2af9a63e7cac9ae128da76d338115
OutlawCyberCode <joshuaklocke47@gmail.com>
2021-06-24 16:50:13 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require("http")
+
+function onRequest(request, response){
+    response.writeHead(200,{"content-type":"text/plain"})
+    response.write("hello world :D")
+    response.end()
+}
+http.createServer(onRequest).listen(8000)
